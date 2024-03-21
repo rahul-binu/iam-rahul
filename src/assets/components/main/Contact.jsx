f4ce472b1124d836485db5c68fedfef452a08be4
rahul-binu <just.commit.it@gmail.com>
2024-03-21 09:26:12 +0530

    minor changes in contact

--- src/assets/components/main/Contact.jsx
+++ src/assets/components/main/Contact.jsx
@@ -26,24 +26,41 @@ const Contact = () => {
         <div>
             <section>
                 <div className={`row- m-2 mb-4 ${styles.mainDiv}`}>
-                    <div className={`col- mx-2 p-3 ${styles.div1} ${styles.vDiv}`}>
-                        <Icon.Whatsapp />
-                    </div>
-                    <div className={`col- mx-2 p-2 ${styles.div2} ${styles.vDiv}`}>
-                        <Icon.Instagram />
-                    </div>
-                    <div className={`col- mx-2 p-2 ${styles.div3} ${styles.vDiv}`}>
-                        <Icon.Facebook />
-                    </div>
-                    <div className={`col- mx-2 p-2 ${styles.div4} ${styles.vDiv}`}>
-                        <Icon.Linkedin />
-                    </div>
-                    <div className={`col- mx-2 p-2 ${styles.div5} ${styles.vDiv}`}>
-                        <Icon.Envelope />
-                    </div>
-                    <div className={`col- mx-2 p-2 ${styles.div6} ${styles.vDiv}`}>
-                        <Icon.Github />
-                    </div>
+                    <a href="">
+                        <div className={`col- mx-2 p-2 ${styles.div1} ${styles.vDiv}`}>
+                            <Icon.Whatsapp />
+                        </div>
+                    </a>
+
+                    <a href="https://www.instagram.com/rahul._.binu/">
+                        <div className={`col- mx-2 p-2 ${styles.div2} ${styles.vDiv}`}>
+                            <Icon.Instagram />
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div className={`col- mx-2 p-2 ${styles.div3} ${styles.vDiv}`}>
+                            <Icon.Facebook />
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div className={`col- mx-2 p-2 ${styles.div4} ${styles.vDiv}`}>
+                            <Icon.Linkedin />
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div className={`col- mx-2 p-2 ${styles.div5} ${styles.vDiv}`}>
+                            <Icon.Envelope />
+                        </div>
+                    </a>
+
+                    <a href="">
+                        <div className={`col- mx-2 p-2 ${styles.div6} ${styles.vDiv}`}>
+                            <Icon.Github />
+                        </div>
+                    </a>
                 </div>
             </section >
         </div >
