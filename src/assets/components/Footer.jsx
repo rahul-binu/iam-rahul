6d25083b043d11ea1ed57ffe4318bd49f7ceb22d
rahul-binu <just.commit.it@gmail.com>
2024-04-10 14:47:20 +0530

    minor changes

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
                                 </svg>
                             </div>
                             <div className="contact-box__info">
-                                <a href="#" className="contact-box__info--title">+91 859019926*</a>
+                                <a className="contact-box__info--title">+91 859019926*</a>
                                 <p className="contact-box__info--subtitle">  Mon-Fri 9am-6pm</p>
                             </div>
                         </div>
@@ -29,7 +29,7 @@ const Footer = () => {
                         <div className="col-md-4 col-lg-4 contact-box pt-1 d-md-block d-lg-flex d-flex">
                             <div className="contact-box__icon">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-mail-opened" viewBox="0 0 24 24" strokeWidth="1" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                    <path d="M0 0h24v24H0z" fill="none" />
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                     <polyline points="3 9 12 15 21 9 12 3 3 9" />
                                     <path d="M21 9v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10" />
                                     <line x1="3" y1="19" x2="9" y2="13" />
@@ -45,7 +45,7 @@ const Footer = () => {
                         <div className="col-md-4 col-lg-4 contact-box pt-1 d-md-block d-lg-flex d-flex">
                             <div className="contact-box__icon">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-map-2" viewBox="0 0 24 24" strokeWidth="1" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                    <path d="M0 0h24v24H0z" fill="none" />
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                     <line x1="18" y1="6" x2="18" y2="6.01" />
                                     <path d="M18 13l-3.5 -5a4 4 0 1 1 7 0l-3.5 5" />
                                     <polyline points="10.5 4.75 9 4 3 7 3 20 9 17 15 20 21 17 21 15" />
@@ -54,7 +54,7 @@ const Footer = () => {
                                 </svg>
                             </div>
                             <div className="contact-box__info">
-                                <a href="#" className="contact-box__info--title">Idukki Kerala</a>
+                                <a  className="contact-box__info--title">Idukki Kerala</a>
                                 <p className="contact-box__info--subtitle">685563, India</p>
                             </div>
                         </div>
