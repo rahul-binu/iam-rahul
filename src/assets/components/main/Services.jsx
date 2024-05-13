f7adb5e7de986b4f91298b52c866bd76a03e4fbb
rahul-binu <just.commit.it@gmail.com>
2024-05-13 06:23:35 +0530

    minor changes

--- src/assets/components/main/Services.jsx
+++ src/assets/components/main/Services.jsx
@@ -63,7 +63,7 @@ const Services = () => {
                         </div>
                     </div>
                     {/* <!-- START THE CS CONTENT  --> */}
-                    <div className="row" >
+                    {/* <div className="row" >
                         <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 services mt-4 text-end">
                             <div className="services__pic">
                                 <img src="images/cyber.png" alt="cloud hosting illustration" className="img-fluid" />
@@ -79,14 +79,14 @@ const Services = () => {
                                     As a cybersecurity specialist, I prioritize the protection of valuable assets and data integrity. With the ever-evolving threat landscape in mind, I implement robust security measures and protocols to safeguard against cyber threats and vulnerabilities. From proactive risk assessments to incident response planning, I work tirelessly to ensure that my clients' digital environments remain secure and resilient.
                                     By fostering a culture of cybersecurity awareness and preparedness, I empower businesses to mitigate risks and thrive in today's digital landscape.
                                 </p>
-                                {/* <button type="button" className="rounded-pill btn-rounded border-primary">Learn more
+                                <button type="button" className="rounded-pill btn-rounded border-primary">Learn more
                                     <span><i className="fas fa-arrow-right"></i></span>
-                                </button> */}
+                                </button>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                     {/* <!-- START THE Bussin CONTENT  --> */}
-                    <div className="row" >
+                    {/* <div className="row" >
                         <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 services mt-4 d-flex align-items-center">
                             <div className="services__content">
                                 <div className="icon d-block fas fa-paper-plane mx-3 service--icon">
@@ -97,9 +97,9 @@ const Services = () => {
                                     As a seasoned professional in business analysis, I specialize in uncovering actionable insights and strategies that drive business growth and efficiency. With a keen eye for detail and a strategic mindset, I collaborate closely with clients to identify key challenges, opportunities, and goals.
                                     By leveraging data-driven analysis and industry best practices, I provide invaluable recommendations and solutions that empower businesses to make informed decisions and achieve sustainable success.
                                 </p>
-                                {/* <button type="button" className="rounded-pill btn-rounded border-primary">Learn more
+                                <button type="button" className="rounded-pill btn-rounded border-primary">Learn more
                                     <span><i className="fas fa-arrow-right"></i></span>
-                                </button> */}
+                                </button>
                             </div>
                         </div>
                         <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 services mt-4 text-end">
@@ -107,7 +107,7 @@ const Services = () => {
                                 <img src="images/bis.png" alt="marketing illustration" className="img-fluid" />
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </section>
         </>
